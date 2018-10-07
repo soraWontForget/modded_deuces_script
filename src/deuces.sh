12c5edadaaf34bc403816bb42b3d8014cb8e0210
soraWontForget <sora.corrumpit@gmail.com>
2018-10-07 15:21:05 -0700

    Improved readability.
    No more clears.
    
    Bootloader and radio now flashed together on the active partition slot.
    
    Added instructions in script for Bootloader and Radio flashing.
    
    End of script prompts should now work.
    
    Added sleep commands to ensure commands are executed successfully after the previous command finishes.

--- src/deuces.sh
+++ src/deuces.sh
@@ -2,18 +2,23 @@
 
 f=$(which fastboot)
 
-clear
+
 
 ## Opening statements
 openingStatement(){
+echo -en "\n"
+echo -en "\n"
 echo "soraWontForget's small modifications to:"
 echo "Deuces-flash-all-script-V4.4-Linux"
 echo "for Taimen & Walleye - Google Pixel 2 / XL"
 echo "USE AT YOUR OWN RISK"
 echo "THIS HAS NOT BEEN TESTED ON MAC / OSX"
 read -n1 -r -p "Press any key to continue..." key
-clear
+echo -en "\n"
+echo -en "\n"
 
+echo -en "\n"
+echo -en "\n"
 echo "Make sure you've changed your working directory to the"
 echo "directory where your archived factory image resides"
 echo "Make sure the archive is the only archive in the directory."
@@ -21,13 +26,17 @@ echo "before you execute this script."
 echo "There's no need to extract the archive yourself."
 echo "The script will take care of that for you."
 read -n1 -r -p "Press any key to continue..." key
-clear
+echo -en "\n"
+echo -en "\n"
 
-echo "Make Sure your Device is in Fastboot Mode"
+echo -en "\n"
+echo -en "\n"
+echo "Make sure your Device is in Fastboot Mode"
 echo "(Power off, hold Volume-Down, hold Power)"
 echo "Once you are in fastboot,"
 read -n1 -r -p "Press any key to continue..." key
-clear
+echo -en "\n"
+echo -en "\n"
 }
 
 ##Prep
@@ -36,6 +45,8 @@ prep(){
 archive=$(ls | grep *.zip)
 newFolderDirectory=$(ls | grep *.zip | sed 's/-factory.*//')
 
+echo -en "\n"
+echo -en "\n"
 unzip $archive */bootloader* */image* */radio*
 pushd $newFolderDirectory
     mkdir images/
@@ -43,13 +54,16 @@ pushd $newFolderDirectory
     # Get list of images
     images=$(ls images/ | sed 's/system.*//' | sed '/^$/d')
 popd
-clear
 }
 
 ##choices-Bootloader-Unlocking
 unlockBootloader(){
+echo -en "\n"
+echo -en "\n"
 read -p "Unlock Bootloader? (y/n)" answer
 if echo "$answer" | grep -iq "^y" ;then
+    echo -en "\n"
+    echo -en "\n"
     echo "Running Unlock"
     echo "Look at your device,"
     echo "press up arrow and Power to confirm"
@@ -64,51 +78,78 @@ if echo "$answer" | grep -iq "^y" ;then
     echo "This will say FAILED if already unlocked, or if device=Walleye - ignore"
 	sleep 5
 else
+    echo -en "\n"
+    echo -en "\n"
     echo "Skipping unlock(s)"
 fi
-clear
 }
 
 ## Flash bootloader and radio modem
 flashBootloaderAndRadio(){
 pushd $newFolderDirectory
+    echo -en "\n"
+    echo -en "\n"
+    echo "If the script hangs at:"
+    echo ""< waiting for any device >""
+    echo "Unplug your device from your usb cable,"
+    echo "and then plug it back in."
+    echo -en "\n"
+    read -n 1 -r -p "Press enter to continue." key
+    echo -en "\n"
+    echo -en "\n"
     echo "Flashing Bootloader & Radio A&B..."
+
+    setActiveSlotA
     sudo $f flash bootloader_a bootloader*.img
     sudo $f reboot-bootloader
-    sleep 5
-    sudo $f flash bootloader_b bootloader*.img
-    sudo $f reboot-bootloader
-    sleep 5
+    sleep 1;
     sudo $f flash radio_a radio*.img
     sudo $f reboot-bootloader
-    sleep 5
+    echo -en "\n"
+    echo -en "\n"
+
+    setActiveSlotB
+    sleep 1;
+    sudo $f flash bootloader_b bootloader*.img
+    sudo $f reboot-bootloader
+    sleep 1;
     sudo $f flash radio_b radio*.img
     sudo $f reboot-bootloader
-    sleep 5
+    echo -en "\n"
+    echo -en "\n"
 popd
-clear
+
 }
 
 
 ## Flash images
 flashPartitions(){
 # Flash Partition A
+echo -en "\n"
+echo -en "\n"
 echo "Flashing Partition A..."
+
 setActiveSlotA
+sleep 1;
 for i in $images; do
     sudo $f flash $( echo $i | sed 's/.img//' )_a $newFolderDirectory/images/$i;
+    sleep 2;
 done
+sleep 2;
 sudo flash $f system_a $newFolderDirectory/images/system.img
-clear
 
 # Flash Partition B
+echo -en "\n"
+echo -en "\n"
 echo "Flashing partition B..."
 setActiveSlotB
+sleep 1;
 for i in $images; do
     sudo $f flash $( echo $i | sed 's/.img//' )_b $newFolderDirectory/images/$i;
+    sleep 2;
 done
+sleep 2;
 sudo $f flash system_b $newFolderDirectory/images/system_other.img
-clear
 }
 
 ## Set the active boot partitions
@@ -124,8 +165,9 @@ sudo $f --set-active=b
 
 ## Format user data prompt
 formatUserData(){
-echo -n "FORMAT USERDATA? (y/n)"
-read answer
+echo -en "\n"
+echo -en "\n"
+read -p "FORMAT USERDATA? (y/n)" answer
 if echo "$answer" | grep -iq "^y" ;then
     echo "Wiping UserData.."
     sudo $f format userdata
@@ -138,6 +180,8 @@ echo "DO NOT LOCK THE BOOTLOADER UNLESS YOU ARE SURE IT IS OPERATING PROPERLY"
 
 ## Reboot system prompt
 rebootSystemPrompt(){
+echo -en "\n"
+echo -en "\n"
 read -p "Reboot system? Enter [y/N]" cont
 case $cont in
 	[Yy*]) echo Rebooting to system...
